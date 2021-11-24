468c6b6dfbb99c813d36b8c97ab708e181a64df8
Wagner Riti <wagner.riti2584@gmail.com>
2021-11-24 13:45:18 -0300

    Acrescentada uma mensagem de bom dia

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
